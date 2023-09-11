fcba88179faa7bc93950fb844ca34abe390c2d82
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-11 07:35:54 -0500

    Fixed the methods in function

--- functions.py
+++ functions.py
@@ -1,14 +1,15 @@
 # This function returns the sum of a list 
 def sum_list(data):
     sum = 0
-    for i in range(len(data)):
-        sum += data[i]
+    for i in data:
+        sum += i
+    return sum
 
 
 # This function returns the product of elements of a list 
 
 def product_list(data):
-    product = 0
-    for i in range(len(data)):
-        product *= data[i]
+    product = 1
+    for i in data:
+        product *= i
     return product 
