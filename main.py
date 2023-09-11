ab023b7c35d20f7b537b516896215f022f559f0c
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-11 07:36:16 -0500

    Adjusted main for test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import functions
 
-array = {1,2,3,4,5}
+my_list = [1,2,3,4,5]
 
-print("The sum of the list is " , functions.sum_list(array))
+print("The sum of the list is " , functions.sum_list(my_list))
 
-print ("The product of the list is ", functions.product_list(array))
+print ("The product of the list is ", functions.product_list(my_list))
     
