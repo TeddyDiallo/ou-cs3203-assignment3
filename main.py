8d50f77e077da3b3827d43b263e3506b82db4dc4
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-11 08:10:17 -0500

    Added comments to main for test

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ my_list = [1,2,3,4,5]
 print("The sum of the list is " , functions.sum_list(my_list))
 
 print ("The product of the list is ", functions.product_list(my_list))
+#This test passed
 
 print ("The reversed list of ", my_list, " is " , functions.reverse_list(my_list))
+#This test passed
     
