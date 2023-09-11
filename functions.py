5accc127032a8bdaa3c6420342266eb66c9d6988
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-11 07:22:10 -0500

    Added method product_list

--- functions.py
+++ functions.py
@@ -3,3 +3,12 @@ def sum_list(data):
     sum = 0
     for i in range(len(data)):
         sum += data[i]
+
+
+# This function returns the product of elements of a list 
+
+def product_list(data):
+    product = 0
+    for i in range(len(data)):
+        product *= data[i]
+    return product 
